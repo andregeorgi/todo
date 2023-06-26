56c345bd6bfb6c5b684f497a0ea4a178534089b4
Andrea Ciorban <andrea.ciorban@ASCENDRO.LOCAL>
2023-06-26 13:41:20 +0300

    adjusted button logic

--- src/components/AddButton/AddButton.jsx
+++ src/components/AddButton/AddButton.jsx
@@ -3,8 +3,12 @@ import Button from "@mui/material/Button";
 function AddButton(props) {
   return (
     <div>
-      <Button variant="contained" style={{ backgroundColor: "#212d40" }}>
-        {props.title}
+      <Button
+        variant="contained"
+        style={{ backgroundColor: "#212d40" }}
+        onClick={props.onClick}
+      >
+        {props.label}
       </Button>
     </div>
   );
