909cd2da7d824037970eac65025577fc7659e0c1
Andrea Ciorban <andrea.ciorban@ASCENDRO.LOCAL>
2023-06-26 11:33:49 +0300

    create input component

---
+++ src/components/TextInput/TextInput.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+
+function TextInput() {
+  return (
+    <Box
+      component="form"
+      sx={{
+        "& > :not(style)": { m: 1, width: "25ch" },
+      }}
+      noValidate
+      autoComplete="off"
+    >
+      <TextField id="outlined-basic" label="Add Todo" variant="outlined" />
+    </Box>
+  );
+}
+
+export default TextInput;
