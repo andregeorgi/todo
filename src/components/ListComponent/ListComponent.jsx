fe68e6a1811800990498631e307aeb9c3967d9fb
Andrea Ciorban <andrea.ciorban@ASCENDRO.LOCAL>
2023-06-26 13:40:16 +0300

    handle delete button

--- src/components/ListComponent/ListComponent.jsx
+++ src/components/ListComponent/ListComponent.jsx
@@ -1,37 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import AddButton from "../AddButton";
 import TextInput from "../TextInput";
-import List from "@mui/material/List";
-import ListItem from "@mui/material/ListItem";
-import ListItemText from "@mui/material/ListItemText";
-import Divider from "@mui/material/Divider";
-import Typography from "@mui/material/Typography";
+import Todo from "../Todo/Todo";
 
 function ListComponent() {
+  const [toDo, setTodo] = useState([
+    { numberTodo: 1, content: "shopping", date: "6/11/2023" },
+    { numberTodo: 2, content: "market", date: "11/1/2022" },
+    { numberTodo: 3, content: "cooking", date: "1/16/2023" },
+    { numberTodo: 4, content: "shopping", date: "4/3/2023" },
+  ]);
+
+  function handleAddTodo(newText) {
+    const newItem = {
+      numberTodo: toDo.length + 1,
+      content: newText,
+      date: new Date().toLocaleString,
+    };
+    setTodo([...toDo, newItem]);
+  }
+
+  function handleDeleteTodo() {
+    setTodo([]);
+  }
+
   return (
     <div>
       <TextInput />
-      <AddButton title="Add" />
-      <List sx={{ width: "100%", maxWidth: 360 }}>
-        <ListItem alignItems="flex-start">
-          <ListItemText
-            primary="Number"
-            secondary={
-              <React.Fragment>
-                <Typography
-                  sx={{ display: "inline" }}
-                  component="span"
-                  variant="body2"
-                  color="text.primary"
-                >
-                  Content
-                </Typography>
-              </React.Fragment>
-            }
-          />
-        </ListItem>
-        <Divider variant="inset" component="li" />
-      </List>
+      <AddButton label="Add" onClick={handleAddTodo} />
+      <AddButton label="Delete" onClick={handleDeleteTodo} />
+
+      {toDo.map((item, index) => (
+        <Todo
+          index={item.numberTodo}
+          contentTodo={item.content}
+          date={item.date}
+        />
+      ))}
     </div>
   );
 }
