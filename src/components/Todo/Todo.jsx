8550f51a55024c3b75cc99f1e7d80f04c92a1fac
Andrea Ciorban <andrea.ciorban@ASCENDRO.LOCAL>
2023-06-26 13:39:18 +0300

    create todo card

--- src/components/Todo/Todo.jsx
+++ src/components/Todo/Todo.jsx
@@ -1,7 +1,40 @@
 import React from "react";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemText from "@mui/material/ListItemText";
+import Divider from "@mui/material/Divider";
+import Typography from "@mui/material/Typography";
+import ListItemAvatar from "@mui/material/ListItemAvatar";
 
-function Todo() {
-  return <div></div>;
+function Todo(props) {
+  return (
+    <div>
+      <List sx={{ width: "100%", maxWidth: 360 }}>
+        <ListItem alignItems="flex-start">
+          <ListItemAvatar>
+            <span></span>
+          </ListItemAvatar>
+          <ListItemText
+            primary={"#" + props.index}
+            secondary={
+              <React.Fragment>
+                <Typography
+                  sx={{ display: "inline" }}
+                  component="span"
+                  variant="body2"
+                  color="text.primary"
+                >
+                  {props.contentTodo}
+                </Typography>
+              </React.Fragment>
+            }
+          />
+          <ListItemText secondary={props.date}></ListItemText>
+        </ListItem>
+        <Divider variant="inset" component="li" />
+      </List>
+    </div>
+  );
 }
 
 export default Todo;
